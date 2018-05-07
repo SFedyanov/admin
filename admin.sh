809e344c5083ebe19fb6201c08e4c336f1521d6c
Sergey Fedyanov <sfedyanov@gmail.com>
2018-05-08 00:42:31 +0300

    first menu

--- admin.sh
+++ admin.sh
@@ -1,69 +1,57 @@
 #!/bin/bash
+set -e
+trap "exit 0" TERM
+export TOP_PID=$$
 
-sayHello() {
-echo "Hello!"
-echo "1. Database"
-echo "2. License"
-echo "3. BackUp"
-echo "4. Exit"
-
-#ACTION=4
-
-read -p 'Enter value [4]:' ACTION
-ACTION=${ACTION:-4}
-#echo $ACTION
-
-case "$ACTION" in
-  1)
-    ACTION_VALUE='DATABASE'
-    createDatabase
-  ;;
-  2)
-    ACTION_VALUE='LICENSE'
-  ;;
-  3)
-    ACTION_VALUE='BACKUP'
-  ;;
-  4)
-    ACTION_VALUE='EXIT'
-    exit 0
-  ;;
-esac
-echo $ACTION_VALUE
+main() {
+  action=$(printMenu)
+  #echo $result
+  choiceAction $action
 }
 
-createDatabase() {
-  echo "create database"
-  read -p 'Y/N [N]:' ACTION
-  ACTION=${ACTION:-N}
-  echo $ACTION
-  sayHello
+
+printMenu() {
+  fileList=$(ls bin/* | xargs -n 1 basename | sed -e 's/\..*$//')
+  fileList="$fileList Quit"
+  PS3='Please enter your choice: '
+  local opt=""
+  while true ; do
+    select opt in $fileList
+    do
+      if test "$opt" = "Quit" ;then
+        kill -s TERM $TOP_PID
+      fi
+      if test "$opt" != "" ;then
+        echo $opt
+        return
+      fi
+    done
+  done
 }
 
-sayHello
+choiceAction() {
+  echo "Action... $1"
+  case $1 in
+    activateLicense)
+      ACTION_VALUE='DATABASE'
+      echo "activateLicense"
+    ;;
+    backupDatabase)
+      ACTION_VALUE='LICENSE'
+      echo "backupDatabase"
+    ;;
+    loadDatabase)
+      ACTION_VALUE='BACKUP'
+      echo "loadDatabase"
+    ;;
+    *)
+      ACTION_VALUE='EXIT'
+      echo "Not correct choice"
+      exit 0
+    ;;
 
-usage() { echo "Usage: $0 [-s <45|90>] [-p <string>]" 1>&2; exit 1; }
+  esac
+}
 
-while getopts ":s:p:" o; do
-    case "${o}" in
-        s)
-            s=${OPTARG}
-#            ((s == 45 || s == 90)) || usage
-            ;;
-        p)
-            p=${OPTARG}
-            ;;
-        *)
-            usage
-            ;;
-    esac
-done
-shift $((OPTIND-1))
-
-if [ -z "${s}" ] || [ -z "${p}" ]; then
-#    usage
-echo "Empty var"
-fi
-
-echo "s = ${s}"
-echo "p = ${p}"
+##Here Start point
+main
